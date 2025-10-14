06e2277daf8c1f4274ad012d81274f1e2de18c08
yoshitha33 <yoshithanunna77@gmail.com>
2025-10-14 23:53:16 +0530

    missingnumber

--- Week1.java
+++ Week1.java
@@ -1,32 +1,69 @@
-//cyclic sort 
+// //cyclic sort 
 
 
-import java.util.Arrays;
+// import java.util.Arrays;
 
-public class Week1{
-    public static void main(String args[]){
-        int [] arr={3,2,4,5,1,6,7};
-        cyclicsort(arr);
-        System.out.println(Arrays.toString(arr));
-    }
+// public class Week1{
+//     public static void main(String args[]){
+//         int [] arr={3,2,4,5,1,6,7};
+//         cyclicsort(arr);
+//         System.out.println(Arrays.toString(arr));
+//     }
 
-    static void cyclicsort(int [] arr){
-        int i=0;
-        while (i<arr.length){
-            int correct=arr[i]-1;
-            if (arr[i]!=arr[correct]){
-                swap(arr, i,correct);}
-                else{
-                    i++;
+//     static void cyclicsort(int [] arr){
+//         int i=0;
+//         while (i<arr.length){
+//             int correct=arr[i]-1;
+//             if (arr[i]!=arr[correct]){
+//                 swap(arr, i,correct);}
+//                 else{
+//                     i++;
 
-                }
+//                 }
 
+//             }
+//         }
+
+//         static void swap(int[] arr, int first,int second){
+//             int temp=arr[first];
+//             arr[first]=arr[second];
+//             arr[second]=temp;
+//         }
+//     }
+
+
+
+
+    class Week1 {
+    public int missingNumber(int[] arr) {
+        cyclicSort(arr);
+
+        
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != i) {
+                return i; 
             }
         }
 
-        static void swap(int[] arr, int first,int second){
-            int temp=arr[first];
-            arr[first]=arr[second];
-            arr[second]=temp;
+        
+        return arr.length;
+    }
+
+    static void cyclicSort(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int correct = arr[i];
+            if (arr[i] < arr.length && arr[i] != arr[correct]) {
+                swap(arr, i, correct);
+            } else {
+                i++;
+            }
         }
     }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
