0b446ca866e1384e07e34d6fbf6aa9d56cb8f2f9
yoshitha33 <yoshithanunna77@gmail.com>
2025-10-29 11:32:57 +0530

    151

---
+++ reverseword151.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String reverseWords(String s) {
+       
+        s = s.trim();
+
+        
+        String[] words = s.split("\\s+");
+
+        
+        StringBuilder result = new StringBuilder();
+
+        for (int i = words.length - 1; i >= 0; i--) {
+            result.append(words[i]);
+            if (i != 0) result.append(" "); 
+        }
+
+        
+        return result.toString();
+    }
+}
