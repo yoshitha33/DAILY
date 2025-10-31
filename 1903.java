65b69007d3b4d6359e52bd16dadcdb7b571968c7
yoshitha33 <yoshithanunna77@gmail.com>
2025-10-31 18:49:51 +0530

    1903

---
+++ 1903.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String largestOddNumber(String num) {
+        
+        for (int i = num.length() - 1; i >= 0; i--) {
+            char ch = num.charAt(i);
+
+            if (ch == '1' || ch == '3' || ch == '5' || ch == '7' || ch == '9') {
+                return num.substring(0, i + 1);
+            }
+        }
+
+        return "";
+    }
+}
+
+
+
+
